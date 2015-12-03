6cb0bfbe53ddf4441240d04f03aa366c7a2e9957
Chris Krycho <chris@chriskrycho.com>
2015-12-02 21:20:38 -0500

    Use folder module structure for bonus episodes.

---
+++ src/bonus/_1.rs
@@ -0,0 +1,55 @@
+//! Polyglot Programming Promises and Perils
+//!
+//! Sometimes, you're doing a technical interview, and you just cannot figure
+//! out why your JavaScript function isn't behaving like it should... and then,
+//! prompted by the interviewer, you realize that you're not returning anything.
+//! Polyglot programming is beautiful, and wonderful, and sometimes it bites you
+//! when you aren't looking.
+//!
+//!   - **Date:** November 25, 2015
+//!   - **Subject:** The delights and challenges of knowing (or even just
+//!     learning) lots of languages.
+//!   - **Audio:**
+//!       + [M4A](http://www.podtrac.com/pts/redirect.m4a/cdn.newrustacean.com/bonus_1.m4a)
+//!       + [MP3](http://www.podtrac.com/pts/redirect.mp3/cdn.newrustacean.com/bonus_1.mp3)
+//!       + [Ogg](http://www.podtrac.com/pts/redirect.ogg/cdn.newrustacean.com/bonus_1.ogg)
+//!
+//! <audio title="Modularize this!" controls preload=metadata>
+//!   <source src="http://www.podtrac.com/pts/redirect.m4a/cdn.newrustacean.com/bonus_1.m4a">
+//!   <source src="http://www.podtrac.com/pts/redirect.mp3/cdn.newrustacean.com/bonus_1.mp3">
+//!   <source src="http://www.podtrac.com/pts/redirect.ogg/cdn.newrustacean.com/bonus_1.ogg">
+//! </audio>
+//!
+//! # Links
+//!
+//!   - [Scala][links-1]
+//!   - [F♯][links-2]
+//!   - [Elixir][links-3]
+//!
+//! [links-1]: http://www.scala-lang.org
+//! [links-2]: http://fsharp.org
+//! [links-3]: http://elixir-lang.org
+//!
+//! # Sponsors
+//!
+//!   - reddraggone9
+//!   - [Chris Patti][sponsors-1]
+//!
+//! [sponsors-1]: http://podcastinit.com
+//!
+//! ## Become a sponsor
+//!
+//!   - [Patreon](https://www.patreon.com/newrustacean)
+//!   - [Venmo](https://venmo.com/chriskrycho)
+//!   - [Dwolla](https://www.dwolla.com/hub/chriskrycho)
+//!   - [Cash.me](https://cash.me/$chriskrycho)
+//!
+//! # Follow
+//!
+//!   - New Rustacean:
+//!       + Twitter: [@newrustacean](https://www.twitter.com/newrustacean)
+//!       + App.net: [@newrustacean](https://alpha.app.net/newrustacean)
+//!       + Email: [hello@newrustacean.com](mailto:hello@newrustacean.com)
+//!   - Chris Krycho
+//!       + Twitter: [@chriskrycho](https://www.twitter.com/chriskrycho)
+//!       + App.net: [@chriskrycho](https://alpha.app.net/chriskrycho)
---
+++ src/bonus/mod.rs
@@ -0,0 +1,3 @@
+//! Bonus episodes: shorter, and usually more about community or philosophy.
+
+pub mod _1;
