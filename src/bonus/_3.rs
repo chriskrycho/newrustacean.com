6c5c49d6c2142e756fd7716b2227f87802fd7adf
Chris Krycho <chris@chriskrycho.com>
2016-02-02 19:08:43 -0500

    Add sponsors and follow info to bonus_3.

--- src/bonus/_3.rs
+++ src/bonus/_3.rs
@@ -13,8 +13,48 @@
 //!   <source src="http://www.podtrac.com/pts/redirect.ogg/cdn.newrustacean.com/bonus_3.ogg">
 //! </audio>
 //!
+//!
+//! Notes
+//! -----
+//!
 //! Community is one of the most important parts of a programming language
 //! community, or indeed *any* technical community. In this episode, I talk
 //! a bit about what happens when you don't have a good community, how Rust's
 //! community has done well so far, and then how to keep building a good
 //! community and how to build good things *as* a community.
+//!
+//!
+//! Sponsors
+//! --------
+//!
+//!   - Aleksey Pirogov
+//!   - Chris Palmer
+//!   - [Derek Morr][s3]
+//!   - Hamza Sheikh
+//!   - Luca Schmid
+//!   - Micael Bergeron
+//!   - Ralph Giles ("rillian")
+//!   - reddraggone9
+//!   - [William Roe][s9]
+//!
+//! [s3]: https://twitter.com/derekmorr
+//! [s9]: http://willroe.me
+//!
+//! ### Become a sponsor
+//!
+//!   - [Patreon](https://www.patreon.com/newrustacean)
+//!   - [Venmo](https://venmo.com/chriskrycho)
+//!   - [Dwolla](https://www.dwolla.com/hub/chriskrycho)
+//!   - [Cash.me](https://cash.me/$chriskrycho)
+//!
+//!
+//! Follow
+//! ------
+//!
+//!   - New Rustacean:
+//!       + Twitter: [@newrustacean](https://www.twitter.com/newrustacean)
+//!       + App.net: [@newrustacean](https://alpha.app.net/newrustacean)
+//!       + Email: [hello@newrustacean.com](mailto:hello@newrustacean.com)
+//!   - Chris Krycho
+//!       + Twitter: [@chriskrycho](https://www.twitter.com/chriskrycho)
+//!       + App.net: [@chriskrycho](https://alpha.app.net/chriskrycho)
