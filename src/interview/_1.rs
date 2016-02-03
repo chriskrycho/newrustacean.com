af35f8ce1eb29cd47d8a97f1bafebd0126ea0779
Chris Krycho <chris@chriskrycho.com>
2016-02-02 19:11:56 -0500

    Add stub for notes for interview_1.

--- src/interview/_1.rs
+++ src/interview/_1.rs
@@ -1,3 +1,46 @@
 //! Sean Griffin
 //!
+//! Notes
+//! -----
+//! TODO
+//!
+//!
 //! Links
+//! -----
+//! TODO
+//!
+//!
+//! Sponsors
+//! --------
+//!
+//!   - Aleksey Pirogov
+//!   - Chris Palmer
+//!   - [Derek Morr][s3]
+//!   - Hamza Sheikh
+//!   - Luca Schmid
+//!   - Micael Bergeron
+//!   - Ralph Giles ("rillian")
+//!   - reddraggone9
+//!   - [William Roe][s9]
+//!
+//! [s3]: https://twitter.com/derekmorr
+//! [s9]: http://willroe.me
+//!
+//! ### Become a sponsor
+//!
+//!   - [Patreon](https://www.patreon.com/newrustacean)
+//!   - [Venmo](https://venmo.com/chriskrycho)
+//!   - [Dwolla](https://www.dwolla.com/hub/chriskrycho)
+//!   - [Cash.me](https://cash.me/$chriskrycho)
+//!
+//!
+//! Follow
+//! ------
+//!
+//!   - New Rustacean:
+//!       + Twitter: [@newrustacean](https://www.twitter.com/newrustacean)
+//!       + App.net: [@newrustacean](https://alpha.app.net/newrustacean)
+//!       + Email: [hello@newrustacean.com](mailto:hello@newrustacean.com)
+//!   - Chris Krycho
+//!       + Twitter: [@chriskrycho](https://www.twitter.com/chriskrycho)
+//!       + App.net: [@chriskrycho](https://alpha.app.net/chriskrycho)
