387894e85537d2f91b6abe4437629ffa0509a88f
Chris Krycho <chris@chriskrycho.com>
2017-02-14 16:09:23 -0500

    Publish "Crates You Should Know: clap".

--- src/crates_you_should_know/_2.rs
+++ src/crates_you_should_know/_2.rs
@@ -1,16 +1,16 @@
-//! quick-xml
+//! clap
 //!
-//!   - **Date:** January 8, 2017
-//!   - **Subject:** A pull-parser for reading and writing XML.
+//!   - **Date:** Feburary 14, 2017
+//!   - **Subject:** A Command-Line Argument Parser.
 //!   - **Audio:**
-//!       + [M4A](http://www.podtrac.com/pts/redirect.m4a/cdn.newrustacean.com/csyk_1.m4a)
-//!       + [MP3](http://www.podtrac.com/pts/redirect.mp3/cdn.newrustacean.com/csyk_1.mp3)
-//!       + [Ogg](http://www.podtrac.com/pts/redirect.ogg/cdn.newrustacean.com/csyk_1.ogg)
+//!       + [M4A](http://www.podtrac.com/pts/redirect.m4a/cdn.newrustacean.com/csyk_2.m4a)
+//!       + [MP3](http://www.podtrac.com/pts/redirect.mp3/cdn.newrustacean.com/csyk_2.mp3)
+//!       + [Ogg](http://www.podtrac.com/pts/redirect.ogg/cdn.newrustacean.com/csyk_2.ogg)
 //!
-//! <audio title="quick-xml" controls preload=metadata>
-//!   <source src="http://www.podtrac.com/pts/redirect.m4a/cdn.newrustacean.com/csyk_1.m4a">
-//!   <source src="http://www.podtrac.com/pts/redirect.mp3/cdn.newrustacean.com/csyk_1.mp3">
-//!   <source src="http://www.podtrac.com/pts/redirect.ogg/cdn.newrustacean.com/csyk_1.ogg">
+//! <audio title="clap" controls preload=metadata>
+//!   <source src="http://www.podtrac.com/pts/redirect.m4a/cdn.newrustacean.com/csyk_2.m4a">
+//!   <source src="http://www.podtrac.com/pts/redirect.mp3/cdn.newrustacean.com/csyk_2.mp3">
+//!   <source src="http://www.podtrac.com/pts/redirect.ogg/cdn.newrustacean.com/csyk_2.ogg">
 //! </audio>
 //!
 //! Links
@@ -19,12 +19,25 @@
 //! - [crates.io]
 //! - [docs.rs]
 //! - [GitHub]
-//! - [syntax highlighting] -- example from [lightning-rs]
+//! - [videos]
+//! - [book]
+//! - YAML:
+//!     + example from clap
+//!         * [YAML][clap-yaml]
+//!         * [associated Rust implementation][clap-rs]
+//!     + example from [lightning-rs]:
+//!         * [YAML][lx-yaml]
+//!         * [associated Rust implementation][lx-rs]
 //! 
-//! [crates.io]: https://crates.io/crates/quick-xml
-//! [docs.rs]: https://docs.rs/quick-xml/0.4.2/quick_xml/
-//! [GitHub]: https://github.com/tafia/quick-xml
-//! [syntax highlighting]: https://github.com/chriskrycho/lightning-rs/blob/a1fcd0af2435c5f2fd4d7c4ef3a5a88f89bdd9f5/src/syntax_highlighting.rs
+//! [crates.io]: https://crates.io/crates/clap
+//! [docs.rs]: https://docs.rs/clap/2.20.3/clap/
+//! [GitHub]: https://github.com/kbknapp/clap-rs
+//! [videos]: https://www.youtube.com/playlist?list=PLza5oFLQGTl2Z5T8g1pRkIynR3E0_pc7U
+//! [book]: https://book.clap.rs
+//! [clap-yaml]: https://github.com/kbknapp/clap-rs/blob/master/examples/17_yaml.yml
+//! [clap-rs]: https://github.com/kbknapp/clap-rs/blob/master/examples/17_yaml.rs
+//! [lx-yaml]: https://github.com/chriskrycho/lightning-rs/blob/a1fcd0af2435c5f2fd4d7c4ef3a5a88f89bdd9f5/src/cli.yml
+//! [lx-rs]: https://github.com/chriskrycho/lightning-rs/blob/a1fcd0af2435c5f2fd4d7c4ef3a5a88f89bdd9f5/src/cli.rs
 //! [lightning-rs]: https://github.com/chriskrycho/lightning-rs
 //!
 //! Sponsors
@@ -34,7 +47,7 @@
 //!   - Andreas Fischer
 //!   - Andrew Thompson
 //!   - Ben Whitley
-//!   - Cameron Mochrie
+//!   - [Charlie Egan]
 //!   - [Chris Palmer]
 //!   - [Christopher Giffard]
 //!   - [Daniel Collin]
@@ -47,6 +60,7 @@
 //!   - Luca Schmid
 //!   - Matt Rudder
 //!   - Matthew Piziak
+//!   - [Max Jacobson]
 //!   - Micael Bergeron
 //!   - Ovidiu Curcan
 //!   - [Pascal Hertleif]
@@ -55,19 +69,21 @@
 //!   - Ralph Giles ("rillian")
 //!   - Raph Levien
 //!   - reddraggone9
-//!   - Ryan Ollos
 //!   - Steven Murawksi
+//!   - Tyler Harper
 //!   - Vesa Kaihlavirta
 //!   - Vlad Bezden
 //!   - [William Roe]
 //!   - Zaki
 //!
+//! [Charlie Egan]: https://charlieegan3.com
 //! [Chris Palmer]: http://home.red-oxide.org/
 //! [Christopher Giffard]: http://blog.cgiffard.com
 //! [Daniel Collin]: https://twitter.com/daniel_collin
 //! [Derek Morr]: https://twitter.com/derekmorr
 //! [Jakub "Limeth" Hlusička]: https://github.com/Limeth
 //! [Jupp Müller]: https://de.linkedin.com/in/juppm
+//! [Max Jacobson]: https://twitter.com/maxjacobson
 //! [Pascal Hertleif]: https://pascalhertleif.de/
 //! [Philipp Keller]: https://twitter.com/hansapla
 //! [William Roe]: http://willroe.me
--- src/crates_you_should_know/mod.rs
+++ src/crates_you_should_know/mod.rs
@@ -1,3 +1,4 @@
 //! Brief discussions of important and useful crates around the Rust ecosystem.
 
 pub mod _1;
+pub mod _2;
