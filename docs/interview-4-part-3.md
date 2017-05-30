9e60e9c7c22a28a08e2e3f41c086e05e6829d9ee
Chris Krycho <chris@chriskrycho.com>
2017-05-30 14:40:56 -0600

    Publish Interview 4 Part 3. :tada:

---
+++ docs/interview-4-part-3.md
@@ -0,0 +1,31 @@
+Hello, I'm Chris Krycho, and this is the New Rustacean podcast, a show about the Rust programming language. This is Interview 4, Part 3: Jonathan Turner.
+In this final part of the interview, Jonathan and I talk about future work on the Rust Language Server, on improvements to the Rust language itself, and a bit about Servo. So let's get back into it!
+
+***
+
+So that wraps up a fantastic interview. I'm looking forward to diving back into our teaching episodes (and being back on a more even schedule: the last month has been a whirlwind of travel and house-buying and more).
+
+Thanks to:
+
+- Anthony Deschamps
+- Benham Esfabod
+- Christopher Giffard
+- Chris Palmer
+- Dan Abrams
+- Daniel Collin
+- Matt Rudder
+- Ben Whitley
+- Peter Tillemans
+- Philipp Keller
+- Raph Levien
+- and Vesa Khailavirta
+
+for sponsoring the show this month. If you're interested in sponsoring the show, you can set up recurring contributions at Patreon.com/newrustacean, or give a one-off contribution at any of a number of other services listed on the show website, or if you're a company interested in advertising to developers, get in touch!
+
+### Info
+
+As always, you can find show notes and links for this episode and previous episodes, code samples, and more at NewRustacean.com. You can follow the show on Twitter @newrustacean; you can follow me there @chriskrycho; and you can follow Jonathan there @jntrnr. If you enjoy the show, please tell somebody about it! You can also help others discover the show by rating and reviewing it on iTunes, recommending it in other podcast directories, or sharing it around on whatever social media you use.
+
+I'd love to hear your feedback, as well as suggestions for topics, interviewees, and so on, in the threads for the episode on the Rust user forum or Hacker News. I also love getting email---you can send me a note at hello@newrustacean.com.
+
+Until next time, happy coding!
---
+++ src/interview/_4/part_3.rs
@@ -0,0 +1,165 @@
+//! Part 3: Smoothing the Rust dev story
+//! 
+//!   - **Date:** June 17, 2017
+//!   - **Subject:** Future work on the RLS, in Rust itself, and in Servo.
+//!   - **Audio:**
+//!       + [M4A][m4a]
+//!       + [MP3][mp3]
+//!       + [Ogg][ogg]
+//!
+//! [m4a]: http://www.podtrac.com/pts/redirect.m4a/cdn.newrustacean.com/interview_4_part_3.m4a
+//! [mp3]: http://www.podtrac.com/pts/redirect.mp3/cdn.newrustacean.com/interview_4_part_3.mp3
+//! [ogg]: http://www.podtrac.com/pts/redirect.ogg/cdn.newrustacean.com/interview_4_part_3.ogg
+//!
+//! <audio style="width: 100%" style="width: 100%" title="Jonathan Turner::Part 3" controls preload=metadata>
+//!   <source src="http://www.podtrac.com/pts/redirect.m4a/cdn.newrustacean.com/interview_4_part_3.m4a">
+//!   <source src="http://www.podtrac.com/pts/redirect.mp3/cdn.newrustacean.com/interview_4_part_3.mp3">
+//!   <source src="http://www.podtrac.com/pts/redirect.ogg/cdn.newrustacean.com/interview_4_part_3.ogg">
+//! </audio>
+//!
+//!
+//! Show Notes
+//! ----------
+//! 
+//! Building the Rust Language Service:
+//! 
+//!   - Language Server Protocol plugins
+//!       + [RLS reference VS Code plugin]
+//!       + Kalita Alexey's [vscode-rust]
+//!       + [langserver.org]
+//!   - [The 2017 Rust Roadmap]
+//!       + [Improved match ergonomics around references]
+//!       + [const generics]
+//!           * [RFC #1931]
+//! 
+//! [RLS reference VS Code plugin]: https://github.com/jonathandturner/rls_vscode
+//! [vscode-rust]: https://github.com/editor-rs/vscode-rust
+//! [langserver.org]: http://langserver.org
+//! [The 2017 Rust Roadmap]: https://blog.rust-lang.org/2017/02/06/roadmap.html
+//! [Improved match ergonomics around references]: https://github.com/rust-lang/rust-roadmap/issues/24
+//! [const generics]: https://internals.rust-lang.org/t/lang-team-minutes-const-generics/5090
+//! [RFC #1931]: https://github.com/rust-lang/rfcs/pull/1931
+//! 
+//! 
+//! Working on Servo:
+//! 
+//!   - [Servo]
+//!       + [Windows nightlies]
+//!   - [LLVM]
+//!       + Apple's use on their graphics pipeline:
+//!           - [OpenGL]
+//!           - [Metal]
+//!       + [clang]
+//!       + [Swift]
+//!   - [Project Quantum]
+//!   - [WebKit]
+//!       + [KHTML]
+//!       + [Safari]
+//!   
+//! [Servo]: https://servo.org
+//! [Windows nightlies]: https://blog.servo.org/2017/04/13/windows/
+//! [llvm]: http://llvm.org
+//! [OpenGL]: http://lists.llvm.org/pipermail/llvm-dev/2006-August/006497.html
+//! [Metal]: https://developer.apple.com/metal/metal-shading-language-specification.pdf
+//! [clang]: http://clang.llvm.org
+//! [swift]: https://swift.org
+//! [Project Quantum]: https://medium.com/mozilla-tech/a-quantum-leap-for-the-web-a3b7174b3c12
+//! [WebKit]: https://webkit.org
+//! [KHTML]: https://en.wikipedia.org/wiki/KHTML
+//! [Safari]: https://www.apple.com/safari/
+//! 
+//! 
+//! Sponsors
+//! --------
+//! 
+//!   - [Anthony Deschamps]
+//!   - Anthony Scotti
+//!   - Aleksey Pirogov
+//!   - Andreas Fischer
+//!   - Andrew Thompson
+//!   - Austin LeSure
+//!   - Ben Whitley
+//!   - Benham Esfabod
+//!   - Benjamin Wasty
+//!   - [Charlie Egan]
+//!   - Chris Jones
+//!   - [Chris Palmer]
+//!   - [Christopher Giffard]
+//!   - Dan Abrams
+//!   - [Daniel Collin]
+//!   - [Derek Morr]
+//!   - Eugene Bulkin
+//!   - [Henri Sivonen]
+//!   - [Jakub "Limeth" Hlusička]
+//!   - Jonathan Turner
+//!   - Jordan Henderson
+//!   - [Jupp Müller]
+//!   - Justin Ossevoort
+//!   - [Karl Hobley]
+//!   - Keith Gray
+//!   - Kilian Rault
+//!   - Lachlan Collins
+//!   - Luca Schmid
+//!   - Matt Rudder
+//!   - Matthew Piziak
+//!   - [Max Jacobson]
+//!   - Micael Bergeron
+//!   - Ovidiu Curcan
+//!   - [Pascal Hertleif]
+//!   - [Patrick O'Doherty]
+//!   - Peter Tillemans
+//!   - Philipp Keller
+//!   - Ralph Giles ("rillian")
+//!   - Randy MacLeod
+//!   - Raph Levien
+//!   - reddraggone9
+//!   - Steven Murawksi
+//!   - [Stuart Hinson]
+//!   - Tim Brooks
+//!   - Tom Prince
+//!   - Ty Overby
+//!   - Tyler Harper
+//!   - Vesa Kaihlavirta
+//!   - Warren Harper
+//!   - [William Roe]
+//!   - Zaki
+//!
+//! [Anthony Deschamps]: https://github.com/adeschamps
+//! [Charlie Egan]: https://charlieegan3.com
+//! [Chris Palmer]: http://home.red-oxide.org/
+//! [Christopher Giffard]: http://blog.cgiffard.com
+//! [Daniel Collin]: https://twitter.com/daniel_collin
+//! [Derek Morr]: https://twitter.com/derekmorr
+//! [Henri Sivonen]: https://hsivonen.fi/
+//! [Jakub "Limeth" Hlusička]: https://github.com/Limeth
+//! [Jupp Müller]: https://de.linkedin.com/in/juppm
+//! [Karl Hobley]: https://github.com/kaedroho/
+//! [Max Jacobson]: https://twitter.com/maxjacobson
+//! [Pascal Hertleif]: https://pascalhertleif.de/
+//! [Patrick O'Doherty]: https://twitter.com/patrickod
+//! [Philipp Keller]: https://twitter.com/hansapla
+//! [Stuart Hinson]: http://stuarth.github.io/
+//! [William Roe]: http://willroe.me
+//!
+//! (Thanks to the couple people donating who opted out of the reward tier, as
+//! well. You know who you are!)
+//!
+//! ### Become a sponsor
+//!
+//!   - [Patreon](https://www.patreon.com/newrustacean)
+//!   - [Venmo](https://venmo.com/chriskrycho)
+//!   - [Dwolla](https://www.dwolla.com/hub/chriskrycho)
+//!   - [Cash.me](https://cash.me/$chriskrycho)
+//!   - [Flattr](https://flattr.com/profile/chriskrycho)
+//!   - [PayPal.me](https://paypal.me/chriskrycho)
+//!
+//!
+//! Contact
+//! -------
+//!
+//!   - New Rustacean:
+//!       + Twitter: [@newrustacean](https://www.twitter.com/newrustacean)
+//!       + Email: [hello@newrustacean.com](mailto:hello@newrustacean.com)
+//!   - Chris Krycho
+//!       + GitHub: [chriskrycho](https://github.com/chriskrycho)
+//!       + Twitter: [@chriskrycho](https://www.twitter.com/chriskrycho)
