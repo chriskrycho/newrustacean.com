1ef5be40b2d9d8c1e6145f3ede507ecf33e5658c
Chris Krycho <chris@chriskrycho.com>
2017-08-14 09:02:07 -0400

    Publish Bonus 9.

---
+++ src/bonus/_9.rs
@@ -0,0 +1,140 @@
+//! Open source is mostly just normal people
+//! 
+//!   - **Date:** August 14, 2017
+//!   - **Subject:** My experience with ember-cli-typescript as an example:
+//!     we're all just people muddling along and doing our best.
+//!   - **Audio:**
+//!       + [MP3][mp3]
+//!       + [M4A][m4a]
+//!       + [Ogg][ogg]
+//!
+//! [mp3]: http://www.podtrac.com/pts/redirect.mp3/cdn.newrustacean.com/_9.mp3
+//! [m4a]: http://www.podtrac.com/pts/redirect.m4a/cdn.newrustacean.com/_9.m4a
+//! [ogg]: http://www.podtrac.com/pts/redirect.ogg/cdn.newrustacean.com/_9.ogg
+//!
+//! <audio style="width: 100%" style="width: 100%" title="Bonus 9: Open source is just normal people" controls preload=metadata>
+//!   <source src="http://www.podtrac.com/pts/redirect.mp3/cdn.newrustacean.com/_9.mp3">
+//!   <source src="http://www.podtrac.com/pts/redirect.m4a/cdn.newrustacean.com/_9.m4a">
+//!   <source src="http://www.podtrac.com/pts/redirect.ogg/cdn.newrustacean.com/_9.ogg">
+//! </audio>
+//!
+//!
+//! Links and Notes
+//! ---------------
+//! 
+//! - [ember-cli-typescript]
+//! - [The Bike Shed #118: Nonsense In, Nonsense Out][bike shed]
+//! - Sean's appearance on New Rustacean: [Interview 1]
+//!
+//! [ember-cli-typescript]: https://github.com/typed-ember/ember-cli-typescript
+//! [bike shed]: http://bikeshed.fm/118
+//! [Interview 1]: http://www.newrustacean.com/show_notes/interview/_1/index.html
+//! 
+//! 
+//! Sponsors
+//! --------
+//! 
+//!   - [Anthony Deschamps]
+//!   - Anthony Scotti
+//!   - Aleksey Pirogov
+//!   - Andreas Fischer
+//!   - Andrew Thompson
+//!   - [Behnam Esfahbod]
+//!   - Benjamin Wasty
+//!   - Brent Vatne
+//!   - [Charlie Egan]
+//!   - Chris Jones
+//!   - [Chris Palmer]
+//!   - [Christopher Giffard]
+//!   - Dan Abrams
+//!   - [Daniel Collin]
+//!   - [David W. Allen]
+//!   - [Derek Morr]
+//!   - Eugene Bulkin
+//!   - [Henri Sivonen]
+//!   - [Jakub "Limeth" Hlusička]
+//!   - James Cooper
+//!   - Jonathan Turner
+//!   - Jordan Henderson
+//!   - [Jupp Müller]
+//!   - Justin Ossevoort
+//!   - [Karl Hobley]
+//!   - Keith Gray
+//!   - Kilian Rault
+//!   - Luca Schmid
+//!   - Matt Rudder
+//!   - Matthew Piziak
+//!   - [Max Jacobson]
+//!   - [Messense Lv]
+//!   - Micael Bergeron
+//!   - Oliver Uvman - see the [Tomaka Patreon] and [arewegameyet.com]
+//!   - Ovidiu Curcan
+//!   - [Pascal Hertleif]
+//!   - [Patrick O'Doherty]
+//!   - Peter Tillemans
+//!   - Philipp Keller
+//!   - Ralph Giles ("rillian")
+//!   - Randy MacLeod
+//!   - Raph Levien
+//!   - Raphael
+//!   - [Rob Tsuk]
+//!   - reddraggone9
+//!   - [Sebastián Ramírez Magrí]
+//!   - Steven Murawksi
+//!   - [Stuart Hinson]
+//!   - Tim Brooks
+//!   - Tom Prince
+//!   - Ty Overby
+//!   - Tyler Harper
+//!   - Vesa Kaihlavirta
+//!   - Warren Harper
+//!   - [William Roe]
+//!   - Zaki
+//!
+//! [Anthony Deschamps]: https://github.com/adeschamps
+//! [Behnam Esfahbod]: https://github.com/behnam
+//! [Brent Vatne]: https://github.com/brentvatne
+//! [Charlie Egan]: https://charlieegan3.com
+//! [Chris Palmer]: http://home.red-oxide.org/
+//! [Christopher Giffard]: http://blog.cgiffard.com
+//! [Daniel Collin]: https://twitter.com/daniel_collin
+//! [David Allen]: http://GitHub.com/DataRiot
+//! [Derek Morr]: https://twitter.com/derekmorr
+//! [Henri Sivonen]: https://hsivonen.fi/
+//! [Jakub "Limeth" Hlusička]: https://github.com/Limeth
+//! [Jupp Müller]: https://de.linkedin.com/in/juppm
+//! [Karl Hobley]: https://github.com/kaedroho/
+//! [Max Jacobson]: https://twitter.com/maxjacobson
+//! [Messense Lv]: https://github.com/messense
+//! [Pascal Hertleif]: https://pascalhertleif.de/
+//! [Patrick O'Doherty]: https://twitter.com/patrickod
+//! [Philipp Keller]: https://twitter.com/hansapla
+//! [Rob Tsuk]: https://github.com/rtsuk
+//! [Sebastián Ramírez Magrí]: https://www.twitter.com/sebasmagri
+//! [Stuart Hinson]: http://stuarth.github.io/
+//! [Tomaka Patreon]: https://www.patreon.com/tomaka/
+//! [arewegameyet.com]: http://arewegameyet.com/
+//! [William Roe]: http://willroe.me
+//!
+//! (Thanks to the couple people donating who opted out of the reward tier, as
+//! well. You know who you are!)
+//!
+//! ### Become a sponsor
+//!
+//!   - [Patreon](https://www.patreon.com/newrustacean)
+//!   - [Venmo](https://venmo.com/chriskrycho)
+//!   - [Dwolla](https://www.dwolla.com/hub/chriskrycho)
+//!   - [Cash.me](https://cash.me/$chriskrycho)
+//!   - [Flattr](https://flattr.com/profile/chriskrycho)
+//!   - [PayPal.me](https://paypal.me/chriskrycho)
+//!
+//!
+//! Contact
+//! -------
+//!
+//!   - New Rustacean:
+//!       + Twitter: [@newrustacean](https://www.twitter.com/newrustacean)
+//!       + Email: [hello@newrustacean.com](mailto:hello@newrustacean.com)
+//!   - Chris Krycho
+//!       + GitHub: [chriskrycho](https://github.com/chriskrycho)
+//!       + Twitter: [@chriskrycho](https://www.twitter.com/chriskrycho)
