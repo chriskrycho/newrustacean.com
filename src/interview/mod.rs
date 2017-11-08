65836d3d1531af916c9865399e5026547af93e21
Chris Krycho <chris@chriskrycho.com>
2017-11-08 06:37:05 -0700

    WIP: RBR interviews.

--- src/interview/mod.rs
+++ src/interview/mod.rs
@@ -4,3 +4,5 @@ pub mod _1;
 pub mod _2;
 pub mod _3;
 pub mod _4;
+
+pub mod rbr_2017;
---
+++ src/interview/rbr_2017.rs
@@ -0,0 +1,132 @@
+//! Micro-interviews from Rust Belt Rust 2017
+//!
+//! Links
+//! -----
+//!
+//!
+//! Sponsors
+//! --------
+//!
+//!   - Aaron Turon
+//!   - Alexander Payne
+//!   - [Anthony Deschamps]
+//!   - Anthony Scotti
+//!   - Anton Van Moere
+//!   - Aleksey Pirogov
+//!   - Andreas Fischer
+//!   - Andrew Thompson
+//!   - Austin LeSure
+//!   - [Behnam Esfahbod]
+//!   - Benjamin Wasty
+//!   - Brent Vatne
+//!   - [Charlie Egan]
+//!   - Chris Jones
+//!   - Christian Schwarz
+//!   - [Chris Palmer]
+//!   - Dan Abrams
+//!   - [Daniel Collin]
+//!   - [David W. Allen]
+//!   - David Hewson
+//!   - [Derek Morr]
+//!   - Eugene Bulkin
+//!   - [Henri Sivonen]
+//!   - [Ian Jones]
+//!   - [Jakub "Limeth" Hlusička]
+//!   - James Cooper
+//!   - Jerome Froelich
+//!   - Jonathan Turner
+//!   - [Jupp Müller]
+//!   - Justin Ossevoort
+//!   - [Karl Hobley]
+//!   - Keith Gray
+//!   - Kilian Rault
+//!   - Luca Schmid
+//!   - Masashi Fujita
+//!   - Matt Rudder
+//!   - Matthew Bettcher
+//!   - Matthew Brenner
+//!   - Matthew Piziak
+//!   - Matthias Ruszala
+//!   - [Max Jacobson]
+//!   - [Messense Lv]
+//!   - Micael Bergeron
+//!   - [Nathan Sculli]
+//!   - [Nick Stevens]
+//!   - Ovidiu Curcan
+//!   - [Pascal Hertleif]
+//!   - [Patrick O'Doherty]
+//!   - Peter Tillemans
+//!   - Peter Zuidhoek
+//!   - Philipp Keller
+//!   - Ralph Giles ("rillian")
+//!   - Randy MacLeod
+//!   - Raph Levien
+//!   - reddraggone9
+//!   - [Ryan Blecher]
+//!   - Sam Whited
+//!   - [Sebastián Ramírez Magrí]
+//!   - Simon G.
+//!   - Steven Murawksi
+//!   - [Stuart Hinson]
+//!   - Tim Brooks
+//!   - Tom Prince
+//!   - Ty Overby
+//!   - Tyler Harper
+//!   - Vesa Kaihlavirta
+//!   - [William Roe]
+//!   - Zachary Snyder
+//!   - Zaki
+//!
+//! [Anthony Deschamps]: https://github.com/adeschamps
+//! [Behnam Esfahbod]: https://github.com/behnam
+//! [Brent Vatne]: https://github.com/brentvatne
+//! [Charlie Egan]: https://charlieegan3.com
+//! [Chris Palmer]: http://home.red-oxide.org/
+//! [Daniel Collin]: https://twitter.com/daniel_collin
+//! [David Allen]: http://GitHub.com/DataRiot
+//! [Derek Morr]: https://twitter.com/derekmorr
+//! [Henri Sivonen]: https://hsivonen.fi/
+//! [Ian Jones]: https://www.ianmjones.com/
+//! [Jakub "Limeth" Hlusička]: https://github.com/Limeth
+//! [Jupp Müller]: https://de.linkedin.com/in/juppm
+//! [Karl Hobley]: https://github.com/kaedroho/
+//! [Max Jacobson]: https://twitter.com/maxjacobson
+//! [Messense Lv]: https://github.com/messense
+//! [Nathan Sculli]: http://influential.co/
+//! [Nick Stevens]: https://github.com/nastevens
+//! [Pascal Hertleif]: https://pascalhertleif.de/
+//! [Patrick O'Doherty]: https://twitter.com/patrickod
+//! [Philipp Keller]: https://twitter.com/hansapla
+//! [Ryan Blecher]: http://notryanb.github.io/
+//! [Sebastián Ramírez Magrí]: https://www.twitter.com/sebasmagri
+//! [Stuart Hinson]: http://stuarth.github.io/
+//! [William Roe]: http://willroe.me
+//!
+//! (Thanks to the couple people donating who opted out of the reward tier, as
+//! well. You know who you are!)
+//!
+//! ### Become a sponsor
+//!
+//!   - [Patreon](https://www.patreon.com/newrustacean)
+//!   - [Venmo](https://venmo.com/chriskrycho)
+//!   - [Dwolla](https://www.dwolla.com/hub/chriskrycho)
+//!   - [Cash.me](https://cash.me/$chriskrycho)
+//!   - [Flattr](https://flattr.com/profile/chriskrycho)
+//!   - [PayPal.me](https://paypal.me/chriskrycho)
+//!
+//!
+//! Contact
+//! -------
+//!
+//!   - New Rustacean:
+//!       + Twitter: [@newrustacean](https://www.twitter.com/newrustacean)
+//!       + Email: [hello@newrustacean.com](mailto:hello@newrustacean.com)
+//!   - Chris Krycho
+//!       + GitHub: [chriskrycho](https://github.com/chriskrycho)
+//!       + Twitter: [@chriskrycho](https://www.twitter.com/chriskrycho)
+
+#[doc(include = "../docs/rbr_2017/andrew-hobden.md")]
+pub mod andrew_hobden {}
+
+#[doc(include = "../docs/rbr_2017/anthony-deschamps.md")]
+pub mod anthony_deschamps {}
