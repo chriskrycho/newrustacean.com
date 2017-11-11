551c48f8f7628919793920c53a4a5cfeae3c2e99
Chris Krycho <chris@chriskrycho.com>
2017-11-11 13:35:44 -0700

    Fix typo in Colin Dean.

--- src/interview/rbr_2017/colin_dean.rs
+++ src/interview/rbr_2017/colin_dean.rs
@@ -10,7 +10,7 @@
 //! [MP3]: http://www.podtrac.com/pts/redirect.mp3/cdn.newrustacean.com/interviews/rbr_2017/colin_dean.mp3
 //! [Ogg]: http://www.podtrac.com/pts/redirect.ogg/cdn.newrustacean.com/interviews/rbr_2017/colin_dean.ogg
 //!
-//! <audio style="width: 100%" style="width: 100%" title="Interview 2::Part 2 – Raph Levien" controls preload=metadata>
+//! <audio style="width: 100%" style="width: 100%" title="Rust Belt Rust 2017: Colin Dean" controls preload=metadata>
 //!   <source src="http://www.podtrac.com/pts/redirect.m4a/cdn.newrustacean.com/interviews/rbr_2017/colin_dean.m4a">
 //!   <source src="http://www.podtrac.com/pts/redirect.mp3/cdn.newrustacean.com/interviews/rbr_2017/colin_dean.mp3">
 //!   <source src="http://www.podtrac.com/pts/redirect.ogg/cdn.newrustacean.com/interviews/rbr_2017/colin_dean.ogg">
