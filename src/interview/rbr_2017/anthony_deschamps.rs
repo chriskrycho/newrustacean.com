d0c60fd0687b5e85ceb13e499d05df52fc3068a0
Chris Krycho <chris@chriskrycho.com>
2017-11-13 21:03:05 -0700

    Anthony Deschamps @ RBR 2017.

---
+++ src/interview/rbr_2017/anthony_deschamps.rs
@@ -0,0 +1,96 @@
+//! Anthony Deschamps
+//!
+//!   - **Date:** November 13, 2017 (recorded October 27, 2017)
+//!   - **Audio:**
+//!       + [M4A]
+//!       + [MP3]
+//!       + [Ogg]
+//!
+//! [M4A]: http://www.podtrac.com/pts/redirect.m4a/cdn.newrustacean.com/interview/rbr_2017/anthony_deschamps.m4a
+//! [MP3]: http://www.podtrac.com/pts/redirect.mp3/cdn.newrustacean.com/interview/rbr_2017/anthony_deschamps.mp3
+//! [Ogg]: http://www.podtrac.com/pts/redirect.ogg/cdn.newrustacean.com/interview/rbr_2017/anthony_deschamps.ogg
+//!
+//! <audio style="width: 100%" style="width: 100%" title="Rust Belt Rust 2017: Colin Dean" controls preload=metadata>
+//!   <source src="http://www.podtrac.com/pts/redirect.m4a/cdn.newrustacean.com/interview/rbr_2017/anthony_deschamps.m4a">
+//!   <source src="http://www.podtrac.com/pts/redirect.mp3/cdn.newrustacean.com/interview/rbr_2017/anthony_deschamps.mp3">
+//!   <source src="http://www.podtrac.com/pts/redirect.ogg/cdn.newrustacean.com/interview/rbr_2017/anthony_deschamps.ogg">
+//! </audio>
+
+
+/// **Chris:** hello! Can you tell me your name and a little bit about yourself?
+/// 
+/// **Anthony:** My name is Anthony Deschamps, I, um, I’m a software developer,
+/// I work in Automotive.
+/// 
+/// **Chris:** Oh!  Very interesting. Long-time listeners will recognize
+/// Anthony’s name as a sponsor of the show; thank you for sponsoring the show!
+/// 
+/// **Anthony:** You’re welcome!
+/// 
+/// **Chris:** So, what got you into Rust?
+/// 
+/// **Anthony:** I’ve talked about this earlier; I actually can’t remember how I
+/// first came across it. Um, I remember my friends being excited about it and
+/// looking at it at some point, um, but what really hooked me is that I have a
+/// huge amount of respect for C+ +, uh, it was one of my first languages, and
+/// to me, Rust feels like C+ + with decades of learned lessons. If we have a
+/// clean slate, and what you can do with a fresh start.
+/// 
+/// **Chris:** Yeah. How long - do you remember roughly, obviously you don’t
+/// remember exactly when, but - do you remember roughly how long...pre 1.0,
+/// post 1.0?
+/// 
+/// **Anthony:** Uh, probably about a year ago, so, somewhere after 1.10 or
+/// roundabouts.
+/// 
+/// **Chris:** Okay. Very good. What has your experience of learning Rust been
+/// like? Good, bad, ugly?
+/// 
+/// **Anthony:** Um, it’s made my C+ + better.
+/// 
+/// **Chris:** Yeah.
+/// 
+/// **Anthony:** Everything thing that I struggled with in Rust was really just
+/// a lesson for what I could be doing better in other places.
+/// 
+/// **Chris:** What are you using Rust for presently? Are you able to use it at
+/// work at all, or is it side projects entirely, still?
+/// 
+/// **Anthony:** So, a combination of hobby projects, uh, when I have time. And,
+/// a little bit at work. It’s one of those things where it is a little bit of a
+/// risk, a newer thing, so it’s been nice to try it out on some small things,
+/// see how it goes, and realize that I do like it and get excited about hoping
+/// to use it more.
+/// 
+/// **Chris:** Yeah. What kind of side projects have you been able to do?
+/// 
+/// **Anthony:** Um, when I get around to strapping a Raspberry Pi to a balloon
+/// and sending it up to the stratosphere to take some photos, that’ll be in
+/// Rust.
+/// 
+/// **Chris:** That’s awesome.
+/// 
+/// **Anthony:** I also like to play around with arduinos, and LEDs are fun, and
+/// I’m using a little bit of Rust there.
+/// 
+/// **Chris:** Cool. Is there anything in particular that’s caught your
+/// attention either with this conference or with the Rust community in general?
+/// 
+/// **Anthony:** The most exciting thing to me is meeting the people who are
+/// making the things that I enjoy using. Uh, it seems obvious when you really
+/// think about it, but, um, the things that you use are not made by
+/// some...cloud, or void, or they don’t just come out of nowhere, they’re made
+/// from real people, who really enjoy working on what they’re doing, and are
+/// really excited to talk to you about it.
+/// 
+/// **Chris:** I share that sentiment deeply. Thank you for your time!
+/// 
+/// **Anthony:** Well, thank you so much for the podcast. I really enjoy it.
+/// 
+/// **Chris:** My pleasure, and absolutely awesome, speaking of meeting people
+/// in person, it’s great to meet you in person!
+/// 
+/// **Anthony:** I agree. Thank you so much.
+/// 
+/// **Chris:** Thank you!
+pub struct Transcript;
