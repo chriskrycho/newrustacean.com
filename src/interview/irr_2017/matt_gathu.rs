b5faf4647d56e39d5fd87fa9b2ef5df1785ce7ee
Chris Krycho <chris@chriskrycho.com>
2017-12-30 16:04:50 -0700

    IRR 2017: Matt Gathu

---
+++ src/interview/irr_2017/matt_gathu.rs
@@ -0,0 +1,169 @@
+//! Matt Gathu
+//!
+//!   - **Date:** December 30, 2017
+//!   - **Subject:** Matt's experience porting wget to Rust.
+//!   - **Audio:**
+//!       + [MP3][mp3]
+//!       + [M4A][m4a]
+//!       + [Ogg][ogg]
+//!
+//! [m4a]: http://www.podtrac.com/pts/redirect.m4a/cdn.newrustacean.com/interview/irr_2017/matt_gathu.m4a
+//! [mp3]: http://www.podtrac.com/pts/redirect.mp3/cdn.newrustacean.com/interview/irr_2017/matt_gathu.mp3
+//! [ogg]: http://www.podtrac.com/pts/redirect.ogg/cdn.newrustacean.com/interview/irr_2017/matt_gathu.ogg
+//!
+//! <audio style="width: 100%" style="width: 100%" title="Jonathan Turner::Part 3" controls preload=metadata>
+//!   <source src="http://www.podtrac.com/pts/redirect.m4a/cdn.newrustacean.com/interview/irr_2017/matt_gathu.m4a">
+//!   <source src="http://www.podtrac.com/pts/redirect.mp3/cdn.newrustacean.com/interview/irr_2017/matt_gathu.mp3">
+//!   <source src="http://www.podtrac.com/pts/redirect.ogg/cdn.newrustacean.com/interview/irr_2017/matt_gathu.ogg">
+//! </audio>
+//!
+//!
+//! Show Notes
+//! ----------
+//!
+//! - [Rosetta Code]
+//! - [wget]
+//!     - [Matt's Rust implementation][rget]
+//! - Matt's blog posts
+//!     - [Writing a Command Line Tool in Rust][cli]
+//!     - [Testing a Rust Command Line Tool][test-cli]
+//! - Rust Nairobi
+//!     - [Meetup]
+//!     - [@RustNairobi]
+//!
+//! [Rosetta Code]: https://rosettacode.org/wiki/Category:Programming_Tasks
+//! [wget]: https://www.gnu.org/software/wget/
+//! [rget]: https://github.com/mattgathu/duma
+//! [cli]: http://mattgathu.github.io/writing-cli-app-rust/
+//! [test-cli]: http://mattgathu.github.io/testing-rust-cli-apps/
+//! [Meetup]: https://www.meetup.com/Rust-Nairobi/
+//! [@RustNairobi]: https://twitter.com/RustNairobi
+//!
+//!
+//! Sponsors
+//! --------
+//!
+//!   - Aaron Turon
+//!   - Alexander Payne
+//!   - [Anthony Deschamps]
+//!   - Anthony Scotti
+//!   - Aleksey Pirogov
+//!   - Andreas Fischer
+//!   - Andrew Thompson
+//!   - Austin LeSure
+//!   - [Behnam Esfahbod]
+//!   - Benjamin Wasty
+//!   - Brent Vatne
+//!   - Chap Lovejoy
+//!   - [Charlie Egan]
+//!   - Chris Jones
+//!   - [Chris Palmer]
+//!   - [Coleman McFarland]
+//!   - Dan Abrams
+//!   - [Daniel Collin]
+//!   - Daniel P. Clark
+//!   - [David W. Allen]
+//!   - David Hewson
+//!   - [Derek Morr]
+//!   - Eugene Bulkin
+//!   - [Henri Sivonen]
+//!   - [Ian Jones]
+//!   - [Jakub "Limeth" Hlusička]
+//!   - James Cooper
+//!   - Jerome Froelich
+//!   - [John Rudnick]
+//!   - Jonathan Turner
+//!   - [Jupp Müller]
+//!   - Justin Ossevoort
+//!   - [Karl Hobley]
+//!   - Keith Gray
+//!   - Kilian Rault
+//!   - Luca Schmid
+//!   - Masashi Fujita
+//!   - Matt Rudder
+//!   - Matthew Brenner
+//!   - Matthias Ruszala
+//!   - [Max Jacobson]
+//!   - [Messense Lv]
+//!   - Micael Bergeron
+//!   - [Nathan Sculli]
+//!   - [Nick Stevens]
+//!   - [Oluseyi Sonaiya]
+//!   - Ovidiu Curcan
+//!   - [Pascal Hertleif]
+//!   - [Patrick O'Doherty]
+//!   - Peter Tillemans
+//!   - Ralph Giles ("rillian")
+//!   - Randy MacLeod
+//!   - Raph Levien
+//!   - reddraggone9
+//!   - [Ryan Blecher]
+//!   - [Sebastián Ramírez Magrí]
+//!   - Simon G.
+//!   - Steven Murawksi
+//!   - [Stuart Hinson]
+//!   - Tim Brooks
+//!   - Tom Prince
+//!   - Ty Overby
+//!   - Tyler Harper
+//!   - Vesa Kaihlavirta
+//!   - Victor Kruger
+//!   - Will Greenberg
+//!   - [William Roe]
+//!   - Yaacov Finkelman
+//!   - Zachary Snyder
+//!   - Zaki
+//!
+//! [Anthony Deschamps]: https://github.com/adeschamps
+//! [Behnam Esfahbod]: https://github.com/behnam
+//! [Brent Vatne]: https://github.com/brentvatne
+//! [Charlie Egan]: https://charlieegan3.com
+//! [Chris Palmer]: http://home.red-oxide.org/
+//! [Coleman McFarland]: http://github.com/anxiousmodernman
+//! [Daniel Collin]: https://twitter.com/daniel_collin
+//! [David W. Allen]: http://GitHub.com/DataRiot
+//! [Derek Morr]: https://twitter.com/derekmorr
+//! [Henri Sivonen]: https://hsivonen.fi/
+//! [Ian Jones]: https://www.ianmjones.com/
+//! [Jakub "Limeth" Hlusička]: https://github.com/Limeth
+//! [John Rudnick]: http://www.cindur.com/
+//! [Jupp Müller]: https://de.linkedin.com/in/juppm
+//! [Karl Hobley]: https://github.com/kaedroho/
+//! [Max Jacobson]: https://twitter.com/maxjacobson
+//! [Messense Lv]: https://github.com/messense
+//! [Nathan Sculli]: http://influential.co/
+//! [Nick Stevens]: https://github.com/nastevens
+//! [Oluseyi Sonaiya]: http://oluseyi.info/
+//! [Pascal Hertleif]: https://pascalhertleif.de/
+//! [Patrick O'Doherty]: https://twitter.com/patrickod
+//! [Philipp Keller]: https://twitter.com/hansapla
+//! [Ryan Blecher]: http://notryanb.github.io/
+//! [Sebastián Ramírez Magrí]: https://www.twitter.com/sebasmagri
+//! [Stuart Hinson]: http://stuarth.github.io/
+//! [William Roe]: http://willroe.me
+//!
+//! (Thanks to the couple people donating who opted out of the reward tier, as
+//! well. You know who you are!)
+//!
+//! ### Become a sponsor
+//!
+//!   - [Patreon](https://www.patreon.com/newrustacean)
+//!   - [Venmo](https://venmo.com/chriskrycho)
+//!   - [Dwolla](https://www.dwolla.com/hub/chriskrycho)
+//!   - [Cash.me](https://cash.me/$chriskrycho)
+//!   - [Flattr](https://flattr.com/profile/chriskrycho)
+//!   - [PayPal.me](https://paypal.me/chriskrycho)
+//!
+//!
+//! Contact
+//! -------
+//!
+//!   - New Rustacean:
+//!       + Twitter: [@newrustacean](https://www.twitter.com/newrustacean)
+//!       + Email: [hello@newrustacean.com](mailto:hello@newrustacean.com)
+//!   - Chris Krycho
+//!       + GitHub: [chriskrycho](https://github.com/chriskrycho)
+//!       + Twitter: [@chriskrycho](https://www.twitter.com/chriskrycho)
+
+/// Transcript coming soon!
+pub struct Transcript;
