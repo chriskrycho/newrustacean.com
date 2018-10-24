e67dbce53ed77dfff488d112a3216e5769b24f8f
Chris Krycho <hello@chriskrycho.com>
2018-10-23 21:30:39 -0600

    Update with latest changes from master.

--- implementors/core/ops/arith/trait.Sub.js
+++ implementors/core/ops/arith/trait.Sub.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["show_notes"] = [{text:"impl&lt;'p&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.Sub.html\" title=\"trait core::ops::arith::Sub\">Sub</a> for &amp;'p <a class=\"struct\" href=\"show_notes/e025/struct.Point.html\" title=\"struct show_notes::e025::Point\">Point</a>",synthetic:false,types:["show_notes::e025::Point"]},];
+implementors["show_notes"] = [{text:"impl&lt;'p&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.Sub.html\" title=\"trait core::ops::arith::Sub\">Sub</a>&lt;&amp;'p <a class=\"struct\" href=\"show_notes/e025/struct.Point.html\" title=\"struct show_notes::e025::Point\">Point</a>&gt; for &amp;'p <a class=\"struct\" href=\"show_notes/e025/struct.Point.html\" title=\"struct show_notes::e025::Point\">Point</a>",synthetic:false,types:["show_notes::e025::Point"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
