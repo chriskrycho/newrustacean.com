df1b9b9977e453feea1a1faeadb1d20f2efb6c67
Chris Krycho <hello@chriskrycho.com>
2018-12-31 15:57:50 -0700

    Interview: Integer32.

---
+++ src/interview/integer32.rs
@@ -0,0 +1,230 @@
+//! Integer32: Carol Nichols || Goulding and Jake Goulding
+//! - **Date:** December 31, 2018
+//! - **Subject:** Talking with Carol and Jake about _The Rust Programming
+//!   Language_, _Rust in Motion_, and Rust Belt Rust. With a bonus discussion
+//!   about the value of focusing on our kids over just building big businesses.
+//! - [**download mp3**][mp3]
+//!
+//! [mp3]: https://www.podtrac.com/pts/redirect.mp3/f001.backblazeb2.com/file/newrustacean/interview/integer32.mp3
+//!
+//! <audio style="width: 100%" title="News: Rust 1.29 and 1.30" controls preload=metadata src="https://www.podtrac.com/pts/redirect.mp3/f001.backblazeb2.com/file/newrustacean/interview/integer32.mp3">
+//!
+//! Show Notes
+//! ----------
+//!
+//! Things we mentioned on the show:
+//!
+//! - [the playground]
+//! - [Ferrous Systems]
+//! - Magical JSON: [QuickType]
+//! - EddyB’s company: [Lyken Software Solutions]
+//! - Integer32 blog: [“Tech Conference Budget Case Study: Rust Belt Rust 2018”]
+//!
+//! [the playground]: https://github.com/integer32llc/rust-playground
+//! [Ferrous Systems]: https://ferrous-systems.com
+//! [QuickType]: https://quicktype.io
+//! [Lyken Software Solutions]: https://lyken.rs
+//! [“Tech Conference Budget Case Study: Rust Belt Rust 2018”]: https://integer32.com/2018/11/29/2018-rust-belt-rust-finance-report.html
+//!
+//!
+//! Sponsors
+//! --------
+//!
+//! Thanks to Manning for sponsoring this episode; don’t forget to grab some of
+//! their content at 40% off using the code `podnewrust18`!
+//!
+//! Thanks to Parity for sponsoring the show again. Go check out their [***Rust
+//! jobs***][parity]!
+//!
+//! [parity]: https://paritytech.io/jobs/
+//!
+//! ### Patreon Sponsors
+//!
+//! - Adam Green
+//! - Aleksey Pirogov
+//! - Alexander Kryvomaz
+//! - Alexander Payne
+//! - [Andrew Dirksen]
+//! - Andrew Thompson
+//! - [Anthony Deschamps]
+//! - Anthony Scotti
+//! - [beaorn]
+//! - [Behnam Esfahbod]
+//! - Benjamin Wasty
+//! - Brandon 'Spanky' Mills
+//! - Brian Casiello
+//! - Brian Manning
+//! - [Brian McCallister]
+//! - [Bryan Stitt]
+//! - Caryn Finkelman
+//! - Cass Costello
+//! - Chap Lovejoy
+//! - [Charlie Egan]
+//! - Chip
+//! - [Chris Palmer]
+//! - Dan Abrams
+//! - Daniel
+//! - Daniel Bross
+//! - [Daniel Collin]
+//! - [Daniel Mason]
+//! - David Hewson
+//! - [Derek Morr]
+//! - Doug Reeves
+//! - [Eduard Knyshov]
+//! - [Embark Studios]
+//! - Eugene Bulkin
+//! - [Fabio (decathorpe)]
+//! - Fábio Botelho
+//! - [Gaveen Prabhasara]
+//! - [Graham Wihlidal]
+//! - [Henri Sivonen]
+//! - [Ian Jones]
+//! - "Jake ""ferris"" Taylor"
+//! - Jako Danar
+//! - James Cooper
+//! - James Hagans II
+//! - Jerome Froelich
+//! - [Joar Wandborg]
+//! - [Johan Andersson]
+//! - [John Rudnick]
+//! - Jon
+//! - [Jonathan Knapp]
+//! - Jonathan Turner
+//! - Joseph Hain
+//! - Joseph Marhee
+//! - Justin Ossevoort
+//! - Kai Yao
+//! - Keith Gray
+//! - Kilian Rault
+//! - Lee Jenkins
+//! - Luca Schmid
+//! - [Luiz Irber]
+//! - Lukas Eller
+//! - [Martin Heuschober]
+//! - Masashi Fujita
+//! - Matt Rudder
+//! - Matthew Brenner
+//! - Matthias Ruszala
+//! - [Max Jacobson]
+//! - Max R.R. Collada
+//! - [Messense Lv]
+//! - Micael Bergeron
+//! - [Michael Mc Donnell]
+//! - Michael Melanson
+//! - Michael Sanders
+//! - [Nathan Sculli]
+//! - [Nick Coish]
+//! - Nick Gideo
+//! - [Nick Stevens]
+//! - [Nicolas Pochet]
+//! - Olaf Leidinger    
+//! - Oliver Uvman
+//! - [Oluseyi Sonaiya]
+//! - Ovidiu Curcan
+//! - [Pascal]
+//! - [Patrick O'Doherty]
+//! - Paul Naranja
+//! - Paul Osborne
+//! - Peter Scholtens
+//! - Peter Tillemans
+//! - Pierre-Antoine Champin
+//! - Ralph Giles
+//! - [Ramon Buckland]
+//! - Randy MacLeod
+//! - Raph Levien
+//! - Richard Dallaway
+//! - Rob Tsuk
+//! - Robert Chrzanowski
+//! - [Ryan Blecher]
+//! - [Ryan Osial]
+//! - Scott Moeller
+//! - [Sebastián Ramírez Magrí]
+//! - [Simon Dickson]
+//! - Simon G
+//! - [Steffen Loen Sunde]
+//! - Steve Jenson
+//! - Steven Knight
+//! - Steven Murawski
+//! - [Stuart Hinson]
+//! - Tim Brooks
+//! - Tim Süberkrüb
+//! - Tom Prince
+//! - Toolmaker's Guild
+//! - Ty Overby
+//! - Tyler Harper
+//! - Victor Kruger
+//! - Will Greenberg
+//! - [William Roe]
+//! - Zak van der Merwe
+//! - Zachary Snyder
+//! - Zaki
+//!
+//! [Andrew Dirksen]: https://github.com/bddap
+//! [Anthony Deschamps]: https://github.com/adeschamps
+//! [beaorn]: https://github.com/beaorn
+//! [Behnam Esfahbod]: https://github.com/behnam
+//! [Brian McCallister]: https://skife.org/
+//! [Bryan Stitt]: http://www.stitthappens.com/
+//! [Charlie Egan]: https://charlieegan3.com
+//! [Chris Palmer]: http://home.red-oxide.org/
+//! [Damien Stanton]: https://github.com/damienstanton
+//! [Daniel Collin]: https://twitter.com/daniel_collin
+//! [Daniel Mason]: https://github.com/gisleburt
+//! [Daniel P. Clark]: https://6ftdan.com/
+//! [David W. Allen]: http://GitHub.com/DataRiot
+//! [Derek Morr]: https://twitter.com/derekmorr
+//! [Eduard Knyshov]: https://github.com/edvorg
+//! [Embark Studios]: https://www.embark-studios.com
+//! [Gaveen Prabhasara]: https://twitter.com/gaveen
+//! [Fabio (decathorpe)]: https://decathorpe.com/
+//! [Graham Wihlidal]: https://wihlidal.com/
+//! [Henri Sivonen]: https://hsivonen.fi/
+//! [Ian Jones]: https://www.ianmjones.com/
+//! [Joar Wandborg]: http://github.com/joar
+//! [Johan Andersson]: https://www.embark-studios.com
+//! [Jonathan Knapp]: https://www.coffeeandcode.com/
+//! [John Rudnick]: http://www.cindur.com/
+//! [Luiz Irber]: http://luizirber.org/
+//! [Martin Heuschober]: https://github.com/epsilonhalbe
+//! [Max Jacobson]: https://twitter.com/maxjacobson
+//! [Messense Lv]: https://github.com/messense
+//! [Michael Mc Donnell]: https://www.linkedin.com/in/michaelmcdonnell/
+//! [Nathan Sculli]: http://influential.co/
+//! [Nick Coish]: http://github.com/ncoish
+//! [Nick Stevens]: https://github.com/nastevens
+//! [Nicolas Pochet]: https://github.com/n-pochet
+//! [Oluseyi Sonaiya]: http://oluseyi.info/
+//! [Pascal]: https://pascalhertleif.de/
+//! [Patrick O'Doherty]: https://twitter.com/patrickod
+//! [Philipp Keller]: https://twitter.com/hansapla
+//! [Ramon Buckland]: http://www.inosion.com
+//! [Ryan Blecher]: http://notryanb.github.io/
+//! [Ryan Osial]: https://github.com/osialr
+//! [Sebastián Ramírez Magrí]: https://www.twitter.com/sebasmagri
+//! [Simon Dickson]: https://www.simonhdickson.com/
+//! [Steffen Loen Sunde]: https://www.ntnu.edu/employees/steffen.sunde
+//! [Stuart Hinson]: http://stuarth.github.io/
+//! [William Roe]: http://willroe.me
+//!
+//! (Thanks to the couple people donating who opted out of the reward tier, as
+//! well. You know who you are!)
+//!
+//! ### Become a sponsor
+//!
+//! - [Patreon](https://www.patreon.com/newrustacean)
+//! - [Venmo](https://venmo.com/chriskrycho)
+//! - [Dwolla](https://www.dwolla.com/hub/chriskrycho)
+//! - [Cash.me](https://cash.me/$chriskrycho)
+//! - [Flattr](https://flattr.com/profile/chriskrycho)
+//! - [PayPal.me](https://paypal.me/chriskrycho)
+//!
+//!
+//! Contact
+//! -------
+//!
+//! - New Rustacean:
+//!       + Twitter: [@newrustacean](https://www.twitter.com/newrustacean)
+//!       + Email: [hello@newrustacean.com](mailto:hello@newrustacean.com)
+//! - Chris Krycho
+//!       + GitHub: [chriskrycho](https://github.com/chriskrycho)
+//!       + Twitter: [@chriskrycho](https://www.twitter.com/chriskrycho)
--- src/interview/mod.rs
+++ src/interview/mod.rs
@@ -5,6 +5,7 @@ pub mod _2;
 pub mod _3;
 pub mod _4;
 
-pub mod rbr_2017;
-pub mod irr_2017;
 pub mod diesel_1_0;
+pub mod integer32;
+pub mod irr_2017;
+pub mod rbr_2017;
