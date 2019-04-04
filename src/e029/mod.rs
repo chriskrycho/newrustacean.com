92a584946a7b5cf11ded37897e719904e949dccd
Chris Krycho <hello@chriskrycho.com>
2019-04-03 22:13:53 -0600

    Add e029 links and sponsors.

--- src/e029/mod.rs
+++ src/e029/mod.rs
@@ -25,7 +25,20 @@
 //!
 //! ### Links
 //!
-//!
+//! Other helpful Rust FFI discussions:
+//! 
+//! - [<cite>The Rust Programming Language</cite>’s materials]
+//! - the [nomicon]
+//! - [Rust FFI Omnibus]
+//! - the [Reference’s] discussion
+//! - the associated Rust [API] docs
+//! 
+//! [nomicon]: https://doc.rust-lang.org/beta/nomicon/ffi.html
+//! [Rust FFI Omnibus]: http://jakegoulding.com/rust-ffi-omnibus/
+//! [deals.manning.com/new-rustacean]: http://bit.ly/2OXnlEb
+//! [<cite>The Rust Programming Language</cite>’s materials]: https://doc.rust-lang.org/book/ch19-01-unsafe-rust.html#using-extern-functions-to-call-external-code
+//! [Reference’s]: https://doc.rust-lang.org/1.33.0/reference/items/external-blocks.html
+//! [API]: https://doc.rust-lang.org/1.33.0/std/keyword.extern.html
 //!
 //! Sponsors
 //! --------
@@ -39,7 +52,203 @@
 //!
 //! ### Patreon Sponsors
 //!
-//! TODO: FILL THIS IN.
+//! - Adam Green
+//! - Aleksey Pirogov
+//! - Alexander Kryvomaz
+//! - Alexander Lozada
+//! - Alexander Payne
+//! - [Andrew Dirksen]
+//! - Andrew Thompson
+//! - [Anthony Deschamps]
+//! - Anthony Scotti
+//! - Arlen Haftevani
+//! - [Arlo (Hyena)]
+//! - Arun Kulshreshtha
+//! - [Behnam Esfahbod]
+//! - [Benjamin Manns]
+//! - Benjamin Wasty
+//! - Brandon 'Spanky' Mills
+//! - Brian Casiello
+//! - Brian Manning
+//! - [Brian McCallister]
+//! - [Bryan Stitt]
+//! - Caryn Finkelman
+//! - Cass Costello
+//! - Cat Dad
+//! - Chap Lovejoy
+//! - [Charlie Egan]
+//! - Chip
+//! - [Chris Palmer]
+//! - Christoffer Ceutz
+//! - Dan Abrams
+//! - Daniel
+//! - Daniel Bross
+//! - [Daniel Collin]
+//! - [Daniel Mason]
+//! - David Carroll
+//! - David Hewson
+//! - [Derek Morr]
+//! - Doug Reeves
+//! - [Douglas Correa]
+//! - Edmund Kump
+//! - [Eduard Knyshov]
+//! - [Embark Studios]
+//! - Eugene Bulkin
+//! - [Evan Stoll]
+//! - [Fabio (decathorpe)]
+//! - [Fabio Correa]
+//! - [Gaveen Prabhasara]
+//! - [Graham Wihlidal]
+//! - [Henri Sivonen]
+//! - [Ian Jones]
+//! - Hoàng Đức Hiếu
+//! - Hugo Josefson
+//! - "Jake ""ferris"" Taylor"
+//! - Jako Danar
+//! - James Cooper
+//! - James Hagans II
+//! - [Jason Bowen]
+//! - [Jendrik Illner]
+//! - Jerome Froelich
+//! - [Joar Wandborg]
+//! - [Johan Andersson]
+//! - [John Rudnick]
+//! - Jon
+//! - Jonah
+//! - [Jonathan Knapp]
+//! - Jonathan Turner
+//! - Joseph Hain
+//! - Joseph Mou
+//! - Joseph Schrag
+//! - [Joe Percy]
+//! - Justin Ossevoort
+//! - Kai Yao
+//! - Kazutaka Mise
+//! - Keith Gray
+//! - Kilian Rault
+//! - Lee Jenkins
+//! - Luca Schmid
+//! - [Luiz Irber]
+//! - Lukas Eller
+//! - [Malnormalulo]
+//! - [Martin Heuschober]
+//! - Masashi Fujita
+//! - Matt Rudder
+//! - Matthew Brenner
+//! - Matthias Ruszala
+//! - [Max Jacobson]
+//! - Max R.R. Collada
+//! - [Messense Lv]
+//! - Micael Bergeron
+//! - [Michael Mc Donnell]
+//! - [Michael Melanson]
+//! - Michael Sanders
+//! - [Nathan Sculli]
+//! - [Nick Coish]
+//! - Nick Gideo
+//! - [Nick Stevens]
+//! - [Nicolas Pochet]
+//! - Oladapo Fadeyi
+//! - Olaf Leidinger    
+//! - Oliver Uvman
+//! - [Oluseyi Sonaiya]
+//! - Ovidiu Curcan
+//! - [Pascal]
+//! - [Patrick O'Doherty]
+//! - Paul Naranja
+//! - Paul Osborne
+//! - Peter Scholtens
+//! - Peter Tillemans
+//! - Pierre-Antoine Champin
+//! - Ralph Giles
+//! - [Ramon Buckland]
+//! - Randy MacLeod
+//! - Raph Levien
+//! - Richard Dallaway
+//! - Rob Tsuk
+//! - [Robbie Clarken]
+//! - Robert Chrzanowski
+//! - [Ryan Blecher]
+//! - [Ryan Osial]
+//! - Scott Moeller
+//! - [Sebastián Ramírez Magrí]
+//! - [Simon Dickson]
+//! - Simon G
+//! - [Soren Bramer Schmidt]
+//! - Steve Jenson
+//! - Steven Knight
+//! - Steven Murawski
+//! - [Stuart Hinson]
+//! - Tim Brooks
+//! - Tim Süberkrüb
+//! - Tom Prince
+//! - Toolmaker's Guild
+//! - Ty Overby
+//! - Tyler Harper
+//! - Victor Kruger
+//! - Will Greenberg
+//! - [William Roe]
+//! - Zak van der Merwe
+//! - Zachary Snyder
+//! - Zach Peters
+//! - Zaki
+//!
+//! [Andrew Dirksen]: https://github.com/bddap
+//! [Anthony Deschamps]: https://github.com/adeschamps
+//! [Arlo (Hyena)]: https://asonix.dog/@asonix
+//! [Behnam Esfahbod]: https://github.com/behnam
+//! [Benjamin Manns]: https://www.benmanns.com/
+//! [Brian McCallister]: https://skife.org/
+//! [Bryan Stitt]: http://www.stitthappens.com/
+//! [Charlie Egan]: https://charlieegan3.com
+//! [Chris Palmer]: http://red-oxide.org/
+//! [Damien Stanton]: https://github.com/damienstanton
+//! [Daniel Collin]: https://twitter.com/daniel_collin
+//! [Daniel Mason]: https://github.com/gisleburt
+//! [Daniel P. Clark]: https://6ftdan.com/
+//! [David W. Allen]: http://GitHub.com/DataRiot
+//! [Derek Morr]: https://twitter.com/derekmorr
+//! [Douglas Correa]: http://learnrust.io/
+//! [Eduard Knyshov]: https://github.com/edvorg
+//! [Embark Studios]: https://www.embark-studios.com
+//! [Evan Stoll]: https://github.com/evanjs
+//! [Gaveen Prabhasara]: https://twitter.com/gaveen
+//! [Fabio (decathorpe)]: https://decathorpe.com/
+//! [Fabio Correa]: https://linkedin.com/in/feamcor
+//! [Graham Wihlidal]: https://wihlidal.com/
+//! [Henri Sivonen]: https://hsivonen.fi/
+//! [Ian Jones]: https://www.ianmjones.com/
+//! [Jason Bowen]: https://twitter.com/jwbowen
+//! [Jendrik Illner]: https://www.jendrikillner.com/
+//! [Joar Wandborg]: http://github.com/joar
+//! [Johan Andersson]: https://www.embark-studios.com
+//! [Jonathan Knapp]: https://www.coffeeandcode.com/
+//! [Joe Percy]: http://joetdc.com/
+//! [John Rudnick]: http://www.cindur.com/
+//! [Luiz Irber]: http://luizirber.org/
+//! [Malnormalulo]: https://twitter.com/Malnormalulo
+//! [Martin Heuschober]: https://github.com/epsilonhalbe
+//! [Max Jacobson]: https://twitter.com/maxjacobson
+//! [Messense Lv]: https://github.com/messense
+//! [Michael Mc Donnell]: https://www.linkedin.com/in/michaelmcdonnell/
+//! [Michael Melanson]: https://www.michaelmelanson.net
+//! [Nathan Sculli]: http://influential.co/
+//! [Nick Coish]: http://github.com/ncoish
+//! [Nick Stevens]: https://github.com/nastevens
+//! [Nicolas Pochet]: https://github.com/n-pochet
+//! [Oluseyi Sonaiya]: http://oluseyi.info/
+//! [Pascal]: https://pascalhertleif.de/
+//! [Patrick O'Doherty]: https://twitter.com/patrickod
+//! [Philipp Keller]: https://twitter.com/hansapla
+//! [Ramon Buckland]: http://www.inosion.com
+//! [Robbie Clarken]: https://github.com/RobbieClarken/
+//! [Ryan Blecher]: http://notryanb.github.io/
+//! [Ryan Osial]: https://github.com/osialr
+//! [Sebastián Ramírez Magrí]: https://www.twitter.com/sebasmagri
+//! [Simon Dickson]: https://www.simonhdickson.com/
+//! [Soren Bramer Schmidt]: http://prisma.io/
+//! [Stuart Hinson]: http://stuarth.github.io/
+//! [William Roe]: http://willroe.me
 //!
 //! (Thanks to the couple people donating who opted out of the reward tier, as
 //! well. You know who you are!)
