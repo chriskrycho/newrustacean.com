8ad9dca044132d6ee398a26a9e04c5444c567f4b
Chris Krycho <hello@chriskrycho.com>
2019-05-10 07:18:12 -0600

    e031: make link-against-rust output meaningful.

--- src/e031/e031.c
+++ src/e031/e031.c
@@ -2,4 +2,9 @@
 
 #include "e031.h"
 
-int main() { printf("%d", add_in_rust(123, 456)); }
+int main() {
+    int a = 123;
+    int b = 456;
+    printf("Added `%d` to `%d` in Rust, and got back `%d`", a, b,
+           add_in_rust(123, 456));
+}
