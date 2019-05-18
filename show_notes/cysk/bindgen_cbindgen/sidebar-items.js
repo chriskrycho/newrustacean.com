ef8adc3c9fa4a790cb86368219b8c519894f4124
Chris Krycho <hello@chriskrycho.com>
2019-05-17 20:12:15 -0600

    Update with latest changes from master.

---
+++ show_notes/cysk/bindgen_cbindgen/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["Script","CYSK: `bindgen` and `cbindgen`"]]});
--- show_notes/cysk/sidebar-items.js
+++ show_notes/cysk/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"mod":[["_1","quick-xml"],["_2","clap"],["_3","Rust Language Service"],["rayon","Rayon"],["rocket","Rocket"],["serde","Serde"],["wasm","The wasm tools"]]});
+initSidebarItems({"mod":[["_1","quick-xml"],["_2","clap"],["_3","Rust Language Service"],["bindgen_cbindgen","bindgen and cbindgen"],["rayon","Rayon"],["rocket","Rocket"],["serde","Serde"],["wasm","The wasm tools"]]});
