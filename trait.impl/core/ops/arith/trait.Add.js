5a2c5a741c0a2731dcea69d22c27acd42eed3247
Chris Krycho <hello@chriskrycho.com>
2024-06-17 14:34:49 -0600

    Republish to GH Pages

---
+++ trait.impl/core/ops/arith/trait.Add.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"show_notes":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.Add.html\" title=\"trait core::ops::arith::Add\">Add</a> for <a class=\"struct\" href=\"show_notes/e009/struct.DoubleAndOne.html\" title=\"struct show_notes::e009::DoubleAndOne\">DoubleAndOne</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.Add.html\" title=\"trait core::ops::arith::Add\">Add</a> for <a class=\"struct\" href=\"show_notes/e024/struct.Point.html\" title=\"struct show_notes::e024::Point\">Point</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ trait.impl/core/ops/arith/trait.Sub.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"show_notes":[["impl&lt;'p&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.Sub.html\" title=\"trait core::ops::arith::Sub\">Sub</a> for &amp;'p <a class=\"struct\" href=\"show_notes/e025/struct.Point.html\" title=\"struct show_notes::e025::Point\">Point</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
